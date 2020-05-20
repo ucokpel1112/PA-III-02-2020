f8a54742c5a8fd24e994d20334bd44856ff618ff
helmuth12081999 <helmuthsimon123@gmail.com>
2020-05-20 21:25:50 +0700

    x

---
+++ resources/views/auth/anouncmentMember.blade.php
@@ -0,0 +1,11 @@
+<h1><b>Visit Toba</b></h1>
+<hr>
+{{--Silahkan klik <a href="{{route('konfirmasiemail',["email" => $user->email, "token" => $user->token])}}">Verifikasi Email</a> untuk memverifikasi email anda.--}}
+
+Terima Kasih karena telah mendaftarkan diri anda sebagai member.<br> Selanjutnya kami akan menghubungi anda, dan mengirim informasi terbaru melalui email atau contact lain yang dapat dihubungi.
+<br>
+<br>
+Salam,
+<br>
+<br>
+Visit Toba
