79711dfaa060f6f7f5a7e9c5370537217b9919e4
helmuth12081999 <helmuthsimon123@gmail.com>
2020-05-27 23:04:09 +0700

    x

--- storage/framework/views/7a1a66ed0f946d5c50b73af903df9d5c0b26828d.php
+++ storage/framework/views/7a1a66ed0f946d5c50b73af903df9d5c0b26828d.php
@@ -11,12 +11,11 @@
                                 </a>
                             </div>
                         </div>
-                        <div class="col-xl-6 col-lg-6">
+                        <div class="col-xl-7 col-lg-7">
                             <div class="main-menu  d-none d-lg-block">
                                 <nav>
                                     <ul id="navigation">
                                         <li><a class="active" href="/">Home</a></li>
-                                        <li><a href="about.html">About</a></li>
                                         <li><a href="#">Paket Wisata<i class="ti-angle-down"></i></a>
                                             <ul class="submenu">
                                                 <li><a href="<?php echo e(route('paket')); ?>">Paket Wisata</a></li>
@@ -29,7 +28,7 @@
                                 </nav>
                             </div>
                         </div>
-                        <div class="col-xl-4 col-lg-4 d-none d-lg-block">
+                        <div class="col-xl-3 col-lg-3 d-none d-lg-block">
                             <div class="social_wrap d-flex align-items-center justify-content-end">
 
                                 <div class="social_links d-none d-xl-block">
