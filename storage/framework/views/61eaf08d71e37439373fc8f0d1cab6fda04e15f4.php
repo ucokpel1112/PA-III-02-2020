558694ce7213cb4de82f32aacc07f984fbcea467
helmuth12081999 <helmuthsimon123@gmail.com>
2020-06-08 18:05:07 +0700

    countdown

---
+++ storage/framework/views/61eaf08d71e37439373fc8f0d1cab6fda04e15f4.php
@@ -0,0 +1,71 @@
+<?php $__env->startSection('content'); ?>
+
+    <!-- bradcam_area  -->
+    <div class="bradcam_area bradcam_bg_4">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-12">
+                    <div class="bradcam_text text-center">
+                        <h3>Pesanan Saya</h3>
+                        <p>Riwayat Pemesanan yang Sudah Pernah Anda Lakukan</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Start Align Area -->
+    <div class="whole-wrap">
+        <div class="container box_1170">
+
+            <div class="section-top-border">
+                <h3 class="mb-30">Riwayat Pemesanan</h3>
+                <div class="progress-table-wrap">
+                    <div class="progress-table">
+                        <div class="table-head">
+                            <div class="serial">#</div>
+                            <div class="percentage">Nama Paket Wisata</div>
+                            <div class="country">Status Pemesanan</div>
+                            <div class="visit">Jumlah Peserta Perjalanan</div>
+                            <div class="visit"></div>
+                        </div>
+
+                        <?php $__empty_1 = true; $__currentLoopData = $pemesanan; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $index => $row): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); $__empty_1 = false; ?>
+                            <div class="table-row">
+                                <div class="serial">
+                                    <?php echo e($index+1); ?></div>
+                                <div class="percentage">
+                                    <?php echo e($row->getSesi->getPaket->nama_paket); ?>
+
+                                </div>
+                                <div class="country">
+                                    <?php echo e($row->defineStatus($row->status)); ?>
+
+                                </div>
+                                <div class="visit">
+                                    <?php echo e($row->jumlah_peserta); ?>
+
+                                </div>
+                                <div class="visit">
+                                    <a href="<?php echo e(route('pemesanan.detail',$row->id_pemesanan)); ?>"
+                                       class="btn btn-success">Detail</a>
+                                </div>
+                            </div>
+                        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); if ($__empty_1): ?>
+                            <div class="table-row">
+                                <div class="serial"></div>
+                                <div class="percentage"></div>
+                                <div class="country"> Anda Belum Memiliki Riwayat Pemesanan</div>
+                                <div class="visit"></div>
+                                <div class="visit"></div>
+                            </div>
+                        <?php endif; ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layout.front.main', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\New folder\PA-III-02-2020\resources\views/front/Pemesanan/pemesanan.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/a4367003e3d3c8c6a87640162d28499e1e4a90f5.php
@@ -0,0 +1,103 @@
+<?php $__env->startSection('content'); ?><div class="container">
+    <div class="row justify-content-center" style="opacity: 80%">
+        <div class="col-md-8">
+            <!-- <div class="alert alert-success">
+                    <i class="fa fa-info-circle"></i>Silahkan periksa email untuk melihat verifikasi email anda.
+            </div> -->
+            <?php if(session('info')): ?>
+                <div class="alert alert-success">
+                    <i class="fa fa-info-circle"></i><?php echo e(session('info')); ?>
+
+                </div>
+            <?php endif; ?>
+            <div class="card" style=" background-color: #2F4F4F">
+                <center><div class="card-header"><img src="<?php echo e(asset('img/Login.png')); ?>"
+                                                      alt="CBT Logo"
+                                                      class="brand-image img-circle elevation-3"
+                                                      style="width: 150px"></div></center>
+
+                <div class="card-body">
+                    <form method="POST" action="<?php echo e(route('login')); ?>">
+                        <?php echo csrf_field(); ?>
+
+                        <div class="form-group row">
+                            <label for="email" class="col-md-3 col-form-label text-md-right"></label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control <?php if ($errors->has('email')) :
+if (isset($message)) { $messageCache = $message; }
+$message = $errors->first('email'); ?> is-invalid <?php unset($message);
+if (isset($messageCache)) { $message = $messageCache; }
+endif; ?>" name="email" value="<?php echo e(old('email')); ?>" required autocomplete="email" autofocus placeholder="E-Mail Address">
+
+                                <?php if ($errors->has('email')) :
+if (isset($message)) { $messageCache = $message; }
+$message = $errors->first('email'); ?>
+                                <span class="invalid-feedback" role="alert">
+                                        <strong><?php echo e($message); ?></strong>
+                                    </span>
+                                <?php unset($message);
+if (isset($messageCache)) { $message = $messageCache; }
+endif; ?>
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password" class="col-md-3 col-form-label text-md-right"></label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control <?php if ($errors->has('password')) :
+if (isset($message)) { $messageCache = $message; }
+$message = $errors->first('password'); ?> is-invalid <?php unset($message);
+if (isset($messageCache)) { $message = $messageCache; }
+endif; ?>" name="password" required autocomplete="current-password" placeholder="Password">
+
+                                <?php if ($errors->has('password')) :
+if (isset($message)) { $messageCache = $message; }
+$message = $errors->first('password'); ?>
+                                <span class="invalid-feedback" role="alert">
+                                        <strong><?php echo e($message); ?></strong>
+                                    </span>
+                                <?php unset($message);
+if (isset($messageCache)) { $message = $messageCache; }
+endif; ?>
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <div class="col-md-6 offset-md-3">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" <?php echo e(old('remember') ? 'checked' : ''); ?>>
+
+                                    <label class="form-check-label" for="remember" style="color: #ffffff">
+                                        <?php echo e(__('Remember Me')); ?>
+
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-8 offset-md-3">
+                                <button type="submit" class="btn btn-primary" style="background-color: #2E8B57">
+                                    <?php echo e(__('Login')); ?>
+
+                                </button>
+
+                                <?php if(Route::has('password.request')): ?>
+                                    <a class="btn btn-link" href="<?php echo e(route('password.request')); ?>" style="color: #87CEEB">
+                                        <?php echo e(__('Forgot Your Password?')); ?>
+
+                                    </a>
+                                <?php endif; ?>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layout.auth.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\New folder\PA-III-02-2020\resources\views/auth/login.blade.php ENDPATH**/ ?>
